c7213a97b00d7e10ad6a7103fae10e1f494f574e
Cattit <ekaterina.titaeva@gmail.com>
2019-02-04 11:42:38 +0800

    WTR-13; add data in db

--- data/gismeteo.js
+++ data/gismeteo.js
@@ -1,6 +1,8 @@
 const puppeteer = require('puppeteer');
+let dal = require("wtr-dal");
 const source = "gismeteo"
-const urlDop = [null, "tomorrow/", null, "4-day/", null, "6-day/"]
+const urlDop = [null, "/tomorrow/", null, "/4-day/", null, "/6-day/"]
+const dateNow = new Date()
 
 function plusminus(mas) {
   const regexp = /^(([\u002D\u00AD\u058A\u05BE\u1400\u1806\u2010\u2011\u2012\u2013\u2014\u2015\u2E17\u2E1A\u2E3A\u2E3B\u2E40\u301C\u3030\u30A0\uFE31\uFE32\uFE58\uFE63\uFF0D\u2212\-])|([\+\uFF0B\u002B\u2795]))?\s*(\d*(?:[.,]\d*)?)\s*(\u2103|\u00B0?[СC]|\u2109|\u00B0?F|\u212A|\u00B0?[KК]|\u00B0)?$/mi
@@ -14,31 +16,21 @@ function plusminus(mas) {
 }
 
 function dateDay(amount_day) {
-  let date = new Object()
-  date.now = new Date();
-  date.date_start = new Date();
-  date.date_start.setDate(date.date_start.getDate() + amount_day);
-  date.date_start.setHours(12, 00, 00, 000);
-  date.date_end = new Date();
-  date.date_end.setDate(date.date_end.getDate() + amount_day);
-  date.date_end.setHours(23, 59, 59, 999);
-  date.type = "day"
-
-  return date
+  return {
+    now: dateNow,
+    date_start: new Date(dateNow.getFullYear(), dateNow.getMonth(), dateNow.getDate() + amount_day, 12),
+    date_end: new Date(dateNow.getFullYear(), dateNow.getMonth(), dateNow.getDate() + amount_day, 23, 59, 59, 999),
+    type_day: "day"
+  }
 }
 
 function dateNight(amount_day) {
-  let date = new Object()
-  date.now = new Date();
-  date.date_start = new Date();
-  date.date_start.setDate(date.date_start.getDate() + amount_day);
-  date.date_start.setHours(00, 00, 00, 000);
-  date.date_end = new Date();
-  date.date_end.setDate(date.date_end.getDate() + amount_day);
-  date.date_end.setHours(11, 59, 59, 999);
-  date.type = "night"
-
-  return date
+  return {
+    now: dateNow,
+    date_start: new Date(dateNow.getFullYear(), dateNow.getMonth(), dateNow.getDate() + amount_day, 00),
+    date_end: new Date(dateNow.getFullYear(), dateNow.getMonth(), dateNow.getDate() + amount_day, 11, 59, 59, 999),
+    type_day: "night"
+  }
 }
 
 function fun_rainfall(weather) {
@@ -46,7 +38,7 @@ function fun_rainfall(weather) {
   type.snow = 0;
   type.rain = 0;
   type.sand = 0;
-  type.squalls = 0;
+  type.squall = 0;
   type.mist = 0;
   type.storm = 0;
   type.drizzle = 0;
@@ -68,13 +60,12 @@ function fun_rainfall(weather) {
 
 
 async function getforecast(urlNew) {
-
   let dataAll = [];
+  const browser = await puppeteer.launch();   //запуск браузера хром
+  const page = await browser.newPage();   //переход на новую стр
+  const gismeteo = "https://www.gismeteo.ru/"
   for (let dd = 1, i = 0; dd < 6; dd += 2, i++) {
 
-    const browser = await puppeteer.launch();   //запуск браузера хром
-    const page = await browser.newPage();   //переход на новую стр
-    const gismeteo = "https://www.gismeteo.ru/"
     await page.goto(gismeteo + urlNew + urlDop[dd]); // переход по ссылке
 
     //выполнение скрипта как в консоли Gismeteo
@@ -106,72 +97,43 @@ async function getforecast(urlNew) {
     });
     // .match(/^[а-я]+/ig) слово в начале текста: только вид облачности
 
-    let dataDay = new Object();
-    dataDay.source = source;
-    dataDay.city = urlNew;
-    dataDay.depth_forecast = dd;
-    dataDay.date = dateDay(dataDay.depth_forecast)
-    dataDay.temperature = Math.max(temperature[10], temperature[11], temperature[12], temperature[13])
-    dataDay.wind_speed = new Object();
-    dataDay.wind_speed.from = Math.min(wind_speed[14], wind_speed[15], wind_speed[16], wind_speed[17]);
-    dataDay.wind_speed.to = Math.max(wind_speed[14], wind_speed[15], wind_speed[16], wind_speed[17]);
-    dataDay.wind_gust = Math.max(wind_speed[22], wind_speed[23], wind_speed[24], wind_speed[25]);
-    dataDay.rainfall = fun_rainfall([rainfall[4], rainfall[5], rainfall[6], rainfall[7]]);
-    if (amount_rainfall.length === 0)
-      dataDay.amount_rainfall = 0
-    else
-      dataDay.amount_rainfall = amount_rainfall[4] + amount_rainfall[5] + amount_rainfall[6] + amount_rainfall[7];
+    dataAll.push(
+      {
+        source,
+        url: urlNew,
+        depth_forecast: dd,
+        date: dateDay(dd),
+        temperature: Math.max(temperature[10], temperature[11], temperature[12], temperature[13]),
+        wind_speed: {
+          from: Math.min(wind_speed[14], wind_speed[15], wind_speed[16], wind_speed[17]),
+          to: Math.max(wind_speed[14], wind_speed[15], wind_speed[16], wind_speed[17])
+        },
+        wind_gust: Math.max(wind_speed[22], wind_speed[23], wind_speed[24], wind_speed[25]),
+        rainfall: fun_rainfall([rainfall[4], rainfall[5], rainfall[6], rainfall[7]]),
+        amount_rainfall: amount_rainfall.length === 0 ? 0 : (amount_rainfall[4] + amount_rainfall[5] + amount_rainfall[6] + amount_rainfall[7])
+      })
 
-    dataAll[i] = dataDay;
-    i++;
+    dataAll.push(
+      {
+        source,
+        url: urlNew,
+        depth_forecast: dd,
+        date: dateNight(dd),
+        temperature: Math.min(temperature[6], temperature[7], temperature[8], temperature[9]),
+        wind_speed: {
+          from: Math.min(wind_speed[10], wind_speed[11], wind_speed[12], wind_speed[13]),
+          to: Math.max(wind_speed[10], wind_speed[11], wind_speed[12], wind_speed[13])
+        },
+        wind_gust: Math.max(wind_speed[18], wind_speed[19], wind_speed[20], wind_speed[21]),
+        rainfall: fun_rainfall([rainfall[0], rainfall[1], rainfall[2], rainfall[3]]),
+        amount_rainfall: amount_rainfall.length === 0 ? 0 : (amount_rainfall[0] + amount_rainfall[1] + amount_rainfall[2] + amount_rainfall[3])
+      })
 
-    let dataNight = new Object();
-    dataNight.source = source;
-    dataDay.city = urlNew;
-    dataNight.depth_forecast = dd;
-    dataNight.date = dateNight(dataNight.depth_forecast)
-    dataNight.temperature = Math.min(temperature[6], temperature[7], temperature[8], temperature[9])
-    dataNight.wind_speed = new Object();
-    dataNight.wind_speed.from = Math.min(wind_speed[10], wind_speed[11], wind_speed[12], wind_speed[13]);
-    dataNight.wind_speed.to = Math.max(wind_speed[10], wind_speed[11], wind_speed[12], wind_speed[13]);
-    dataNight.wind_gust = Math.max(wind_speed[18], wind_speed[19], wind_speed[20], wind_speed[21]);
-    dataNight.rainfall = fun_rainfall([rainfall[0], rainfall[1], rainfall[2], rainfall[3]]);
-    if (amount_rainfall.length === 0)
-      dataNight.amount_rainfall = 0
-    else
-      dataNight.amount_rainfall = amount_rainfall[0] + amount_rainfall[1] + amount_rainfall[2] + amount_rainfall[3];
-
-    dataAll[i] = dataNight;
-
-    await browser.close();// закрыть браузер
   }
 
-  console.log(dataAll)
-
-
-
-
-
-
-  // let request = require("request"),
-  //   cheerio = require("cheerio"),
-  //   url = urlNew;
-
-  // request(url, function (error, response, body) {
-  //   if (!error) {
-  //     let $ = cheerio.load(body)
-  //     let temperature = $(".unit_temperature_c").html();
-  //     let num;
-
-  //     if (temperature.indexOf("&#x2212;") != -1)
-  //       num = -1 * parseInt(temperature.replace(/&#x2212;/, "-").replace(/\D+/g, ""));
-  //     else num = parseInt(temperature.replace(/\D+/g, ""));
-
-  //     console.log("Температура " + num + " градусов по Цельсию."); //Math.round(num/10)
-  //   } else {
-  //     console.log("Произошла ошибка: " + error);
-  //   }
-  // });
+  await browser.close();// закрыть браузер
+  // console.log(dataAll)
+  return dataAll
 }
 
 module.exports.getforecast = getforecast;
--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 let getData = require("./data/gismeteo.js");
-getData.getforecast("weather-tel-aviv-5403/"); 
+let dal = require("wtr-dal");
+
+// getData.getforecast("weather-alzamay-145180")
+//     .then(dataAll => {
+//         dal.saveForecast(dataAll[0].source, dataAll[0].date.now)
+//             .then(forecastId => {
+//                 console.log(forecastId);
+// Promise.all(dataAll.map(forecast => dal.saveMeteoData(forecast, forecastId)))
+// }
+// )
+// .then(() => console.log("Successfully sent data from Gismeteo"))
+// .catch(error => console.log("Error sending data from Gismeteo"))
+// })
+async function test() {
+    let url = "weather-alzamay-145180"
+    const dataAll = await getData.getforecast(url);
+    const forecastId = await dal.saveForecast(dataAll[0].source, dataAll[0].date.now);
+    const locationId = await dal.searchLocationByUrl(url)
+    // console.log("GHK")
+    // console.log(forecastId)
+    // console.log(locationId)
+    await dataAll.map(forecast => dal.saveForecastData(forecast, forecastId, locationId));
+}
+
+test();
